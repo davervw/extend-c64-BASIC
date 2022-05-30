61ddcf7ae62d30600d14bdd4e797862b2aae1962
Dave Van Wagner <git@davevw.com>
2022-05-29 20:21:07 -0700

    reverse-string example parsing string in SYS command

---
+++ part-one-reverse-string/README.md
@@ -0,0 +1,18 @@
+# reverse_string example
+
+This example shows how to parse a string expression appended to a SYS command with a comma delimiter.  The source should be relatively self explanatory.
+
+Demo usage:
+
+Mount reverse_string.d64 disk
+
+    LOAD"REVERSE",8,1
+    NEW
+    SYS 49152,"REVERSE ME"
+
+Source code is 6502 assembler targeting Commodore 64
+
+Requires bin/win/[acme.exe](https://sourceforge.net/projects/acme-crossass/) and bin/win/c1541.exe from [Vice](http://vice-emu.sourceforge.net/index.html#download)
+and revise build.sh to use more Vice executables for building and launching
+
+Also see [blog entry](https://techwithdave.davevw.com/2022/05/extending-c64-basic-part-one-parse.html)
---
+++ part-one-reverse-string/code/reverse.asm
@@ -0,0 +1,35 @@
+; reverse.asm
+; Copyright (c) 2022 by David R. Van Wagner
+; davevw.com
+; 
+; Extending C64 BASIC Part One - Parse String
+; https://techwithdave.davevw.com/2022/05/extending-c64-basic-part-one-parse.html
+
+start=$C000 ; machine language org
+
+chrout=$ffd2
+
+syntax_error=$af08
+chkcom=$aefd ; checks for $2c
+frmevl=$ad9e ; evaluate expression
+pulstr=$b6a3 ; pull string from descriptor stack
+
+* = start
+        jsr chkcom
+	jsr frmevl	; evaluate expression
+	bit $d		; string or numeric?
+	bmi +
+        jmp syntax_error
++       jsr pulstr	; pull string from descriptor stack (a=len, x=lo, y=hi addr of string)        
+        stx $fb
+        sty $fc
+        tay
+        lda #18
+        jsr chrout
+-       dey
+        cpy #$ff
+        beq +
+        lda ($fb),y
+        jsr chrout
+        jmp -
++       rts
